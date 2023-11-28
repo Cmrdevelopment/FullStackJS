dfe546a6912f954789a254481c03b52916d33a33
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2023-11-28 09:50:58 +0000

    484

---
+++ Backend/controllers/veterinarioController.js
@@ -0,0 +1,19 @@
+import Veterinario from "../models/Veterinario.js";
+
+
+const registrar = (req, res) => {
+    const { nombre, email, password } = req.body;
+
+    console.log(nombre);
+    console.log(email);
+    console.log(password);
+
+    res.json({ msg: "Registrando usuario" })
+};
+
+const perfil = (req, res) => {
+    res.json({ msg: "Mostrando perfil" });
+}
+
+
+export { registrar, perfil };
--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/bd.js";
+import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 
 const app = express();
+app.use(express.json());
+
 dotenv.config();
 conectarDB();
 
-app.use('/', ( req, res ) => {
-    res.send("Hola Mundo");
-})
+app.use("/api/veterinarios", veterinarioRoutes)
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en el puerto http://localhost:${PORT} `)
 });
-
-process.env.PORT
