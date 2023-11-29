0e390cf040cde9b5d07c24c213aedc3b5513717e
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2023-11-29 17:55:45 +0000

    confirmar, validad usuario, login, autenticarse

--- Backend/controllers/veterinarioController.js
+++ Backend/controllers/veterinarioController.js
@@ -1,14 +1,77 @@
 import Veterinario from "../models/Veterinario.js";
 
 
-const registrar = (req, res) => {
-    const { nombre, email, password } = req.body;
 
-    console.log(nombre);
-    console.log(email);
-    console.log(password);
+const registrar = async (req, res) => {
+    const { email } = req.body;
+    
+    // Prevenir usuarios duplicado
+    const existeUsuario = await Veterinario.findOne({email: email})
 
-    res.json({ msg: "Registrando usuario" })
+    if(existeUsuario) {
+        const error = new Error ("Usuario ya registrado")
+        return res.status(400).json({msg: error.message})
+    }
+
+
+    try {
+        // Guardar un nuevo veterinario
+        const veterinario = new Veterinario(req.body)
+        const veterinarioGuardado = await veterinario.save();
+
+        res.json(veterinarioGuardado)
+
+        
+    } catch (error) {
+        console.log(error);
+    }   
+};
+
+const confirmar = async (req, res) => {
+    const { token } = req.params
+
+    const usuarioConfirmar = await Veterinario.findOne({token: token})
+
+    if(!usuarioConfirmar) {
+        const error = new Error("Token no válido")
+        return res.status(404).json({ msg: error.message })
+    }   
+    
+    try {
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true
+        await usuarioConfirmar.save()
+    
+        res.json({msg: "Usuario Confirmando Correctamente"})
+     } catch (error) {
+        console.log(error);
+    }  
+};
+
+const autenticar = async (req, res) => {
+    const { email, password } = req.body
+
+    // Comprobar si el usuario existe
+    const usuario = await Veterinario.findOne({email})
+
+    if(!usuario) {
+        const error = new Error("El Usuario no existe");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    // Comprobar si el usuario está confirmado
+    if(!usuario.confirmado) {
+        const error = new Error("Tu cuenta no ha sido confirmada");
+        return res.status(403).json({ mes: error.message })
+    }
+
+    // Revisar el password
+    if (await usuario.comprobarPassword(password)) {
+        console.log("Password Correcto")
+    } else {
+        const error = new Error("Tu password es incorrecto");
+        return res.status(403).json({ mes: error.message })
+    }    
 };
 
 const perfil = (req, res) => {
@@ -16,4 +79,4 @@ const perfil = (req, res) => {
 }
 
 
-export { registrar, perfil };
+export { registrar, perfil, confirmar, autenticar };
--- Backend/models/Veterinario.js
+++ Backend/models/Veterinario.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from "bcrypt";
+import generarId from '../helpers/generarid.js';
 
 const veterinarioSchema = mongoose.Schema({
     nombre:{
@@ -26,15 +28,28 @@ const veterinarioSchema = mongoose.Schema({
         default:  null
     },
     token:{
-        type: String
+        type: String,
+        default:  generarId(),
     },
     confirmado:{
         type: Boolean,
         default:  false
     },
-    
-
 });
+
+veterinarioSchema.pre('save', async function(next) {
+   
+    if(!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+veterinarioSchema.methods.comprobarPassword = async function (passwordFormulario) {
+    return await bcrypt.compare(passwordFormulario, this.password)
+}
 
 const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
 export default Veterinario;
