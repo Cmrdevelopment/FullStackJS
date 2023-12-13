6d217a073fb462865cb03d61c7ef8db5c2a6b3c3
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2023-12-13 23:43:01 +0000

    paciente actualizado

--- Backend/controllers/pacienteController.js
+++ Backend/controllers/pacienteController.js
@@ -25,13 +25,17 @@ const obtenerPaciente = async (req, res) => {
         const { id } = req.params;
         const paciente = await Paciente.findById(id);
 
+        if(!paciente) {
+            return res.status(404).json({ msg: 'Paciente no encontrado' });
+        }
+
         if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
             return res.json({ msg: 'Acción no válida' });
         }
 
         if(paciente) {
             res.json(paciente);
-        }
+        } 
 
     } catch (error) {
         console.error(error);
@@ -39,7 +43,39 @@ const obtenerPaciente = async (req, res) => {
     }
 };
 
-const actualizarPaciente = async (req, res) => {};
+const actualizarPaciente = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const paciente = await Paciente.findById(id);
+
+        if(!paciente) {
+            return res.status(404).json({ msg: 'Paciente no encontrado' });
+        }
+
+        if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+            return res.json({ msg: 'Acción no válida' });
+        }
+
+      // Actualizar Paciente
+        paciente.nombre = req.body.nombre || paciente.nombre;
+        paciente.propietario = req.body.propietario || paciente.propietario;
+        paciente.email = req.body.email || paciente.email;
+        paciente.fecha = req.body.fecha || paciente.fecha;
+        paciente.sintomas = req.body.sintomas || paciente.sintomas;
+      try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado);
+    } catch (error) {
+        console.error(error);
+        
+      }
+       
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Error interno del servidor' });
+    }
+};
 
 const eliminarPacienete = async (req, res) => {};
 
