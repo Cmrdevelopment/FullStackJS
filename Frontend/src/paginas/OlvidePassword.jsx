0f4b64d076308c0b517adcc0f7f01d8c3e6d6c24
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2023-12-14 23:53:19 +0000

    iniciar sesión

---
+++ Frontend/src/paginas/OlvidePassword.jsx
@@ -0,0 +1,18 @@
+
+
+const OlvidePassword = () => {
+  return (
+    <>
+    
+        <div>
+            1
+        </div>
+        <div>
+            2
+        </div>
+    
+    </>
+  )
+}
+
+export default OlvidePassword
