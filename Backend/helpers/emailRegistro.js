097185dbe86aa5e1ef37b49967a8d5c4b92e3dea
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2023-12-22 23:17:24 +0000

    nodemailer gmail

--- Backend/helpers/emailRegistro.js
+++ Backend/helpers/emailRegistro.js
@@ -1,14 +1,24 @@
 import nodemailer from 'nodemailer';
 
 const emailRegistro = async (datos) => {
+    // const transporter = nodemailer.createTransport({
+    //     host: process.env.EMAIL_HOST,
+    //     port: process.env.PORT_HOST,
+    //     auth: {
+    //       user: process.env.EMAIL_USER,
+    //       pass: process.env.EMAIL_PASS
+    //     }
+    // });
+
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.PORT_HOST,
+        service: 'gmail',
         auth: {
           user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        }
-    });
+          pass: process.env.EMAIL_PASS,
+        },
+        secure: true, // Usa SSL
+        port: process.env.PORT,    // Puerto SSL
+      });
 
     const { email, nombre, token } = datos;
 
